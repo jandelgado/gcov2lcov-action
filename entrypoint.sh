39c7622dfa4cdab01eabb42bc82e841d816a01a3
Jan Delgado <jdelgado@gmx.net>
2020-09-23 21:07:15 +0200

    use pre-compiled binary of gcov2lcov

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -eu
 
-unset GOROOT
 unset GOPATH
 
 cd $GITHUB_WORKSPACE
-/go/bin/gcov2lcov -infile "$INPUT_INFILE" -outfile "$INPUT_OUTFILE"
+/app/gcov2lcov-linux-amd64 -infile "$INPUT_INFILE" -outfile "$INPUT_OUTFILE"

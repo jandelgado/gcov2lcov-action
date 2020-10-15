b31fa3fb378e9ca2ec92d89dd0a4f55e0aca8064
Septs <github@septs.pw>
2020-10-16 07:43:42 +0800

    cache binary in temporary directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xeuo pipefail
-TMP_BIN=$(mktemp -d -t ci-XXXXXXXXXX)
 NAME="gcov2lcov-linux-amd64"
-wget "https://github.com/jandelgado/gcov2lcov/releases/${VERSION}/download/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "$TMP_BIN"
-chmod +x "$TMP_BIN/$NAME"
-exec "$TMP_BIN/gcov2lcov-linux-amd64" -infile "${INFILE}" -outfile "${OUTFILE}"
+if [ ! -f "/tmp/$NAME" ]; then
+    wget "https://github.com/jandelgado/gcov2lcov/releases/${VERSION}/download/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "/tmp"
+    chmod +x "/tmp/$NAME"
+fi
+exec "/tmp/$NAME" -infile "${INFILE}" -outfile "${OUTFILE}"

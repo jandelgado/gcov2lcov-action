b832f3070e010f52488a1cad7588a8c5db7705b6
Septs <github@septs.pw>
2020-10-16 02:06:14 +0800

    simplify code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
-
 TMP_BIN=$(mktemp -d -t ci-XXXXXXXXXX)
 NAME="gcov2lcov-linux-amd64"
 wget "https://github.com/jandelgado/gcov2lcov/releases/download/${VERSION}/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "$TMP_BIN"
 chmod +x "$TMP_BIN/$NAME"
-cd "$GITHUB_WORKSPACE/$WORKSPACE" || exit 1
 exec "$TMP_BIN/gcov2lcov-linux-amd64" -infile "${INFILE}" -outfile "${OUTFILE}"

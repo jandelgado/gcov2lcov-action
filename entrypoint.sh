b04e6a97e6de87458dfd4f85eea3ec18cb3907c5
Septs <github@septs.pw>
2020-10-16 07:31:47 +0800

    fix entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -xeuo pipefail
 TMP_BIN=$(mktemp -d -t ci-XXXXXXXXXX)
 NAME="gcov2lcov-linux-amd64"
-wget "https://github.com/jandelgado/gcov2lcov/releases/download/${VERSION}/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "$TMP_BIN"
+wget "https://github.com/jandelgado/gcov2lcov/releases/${VERSION}/download/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "$TMP_BIN"
 chmod +x "$TMP_BIN/$NAME"
 exec "$TMP_BIN/gcov2lcov-linux-amd64" -infile "${INFILE}" -outfile "${OUTFILE}"

2fb1238b039653e62857338da6f73b75d972a99e
Septs <github@septs.pw>
2020-10-16 11:39:51 +0800

    remove entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-NAME="gcov2lcov-linux-amd64"
-if [ ! -f "/tmp/$NAME" ]; then
-    wget "https://github.com/jandelgado/gcov2lcov/releases/${VERSION}/download/${NAME}.tar.gz" -q -O - | tar zxf - --strip 1 --directory "/tmp"
-    chmod +x "/tmp/$NAME"
-fi
-exec "/tmp/$NAME" -infile "${INFILE}" -outfile "${OUTFILE}"
